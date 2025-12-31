12ceafd9ee5b9a368cbb192f446abc1522844a40
SKA97 <karmakarsubhrajit680@gmail.com>
2025-12-31 11:54:07 +0530

    Remove unnecessary comments and settings from Baseline SQL dump

--- sql/V1__Baseline.sql
+++ sql/V1__Baseline.sql
@@ -1,29 +1,3 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 17.7
--- Dumped by pg_dump version 18.0
-
--- Started on 2025-12-31 10:34:33
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- TOC entry 6 (class 2615 OID 920173)
--- Name: bantan; Type: SCHEMA; Schema: -; Owner: postgres
---
-
 CREATE SCHEMA bantan;
 
 
@@ -16042,10 +16016,3 @@ ALTER TABLE ONLY master.sub_detail_head
 
 ALTER TABLE ONLY master.sub_major_head
     ADD CONSTRAINT sub_major_head_major_head_id_fkey FOREIGN KEY (major_head_id) REFERENCES master.major_head(id) NOT VALID;
-
-
--- Completed on 2025-12-31 10:34:33
-
---
--- PostgreSQL database dump complete
---
